6bbcfdd40e8e2b36dff08ab44363f2fb6df47724
Sminp <0102262@gmail.com>
2026-05-12 21:56:21 +0900

    feat: 영어 끝말잇기.cpp 리팩토링

--- programmers/박소민/lv2/영어 끝말잇기.cpp
+++ programmers/박소민/lv2/영어 끝말잇기.cpp
@@ -1,54 +1,27 @@
 #include <string>
 #include <vector>
-#include <iostream>
-#include <set>
+#include <unordered_set>
 
 using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
-    int turn = 0;
-    char first;
-    char last;
-    vector<int> answer;
-    set<string> S;
+    unordered_set<string> seen_words;
     
-    for (int i=0; i < words.size(); i++) {
-        if (i % n == 0) {
-            turn++;
+    seen_words.insert(words[0]);
+    
+    for (int i=1; i<words.size(); i++) {
+        const string &curr_word = words[i];
+        const string &prev_word = words[i - 1];
+        
+        if (prev_word.back() != curr_word.front()
+           || seen_words.find(curr_word) != seen_words.end()) {
+            int person = (i%n) +1;
+            int turn = (i/n)+1;
+            return {person, turn};
         }
         
-        const string &val = words.at(i);
-        
-        // 1. 앞 뒤가 같은지 확인
-        if (i == 0) {
-            last = val[val.size()-1];
-        } else {
-            first = val[0];
-            
-            if (first != last) {
-                answer.push_back(i % n + 1);
-                answer.push_back(turn);
-                break;
-            }
-            
-            last = val[val.size()-1];
-            
-        }
-        
-        // 2. 중복되는지 확인
-        if (S.empty() || S.find(val) == S.end()) {
-            S.insert(val);
-        } else {
-            answer.push_back(i % n + 1);
-            answer.push_back(turn);
-            break;
-        }
+        seen_words.insert(curr_word);
     }
     
-    if (answer.empty()) {
-        answer.push_back(0);
-        answer.push_back(0);
-    }
-
-    return answer;
+    return {0, 0};
 }
