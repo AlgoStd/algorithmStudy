7b7622312323efe8f97a26e7db6565fd4752a2b9
Sminp <0102262@gmail.com>
2026-05-19 10:45:11 +0900

    refactor: 박소민 정수삼각형.cpp

--- programmers/박소민/lv3/정수삼각형.cpp
+++ programmers/박소민/lv3/정수삼각형.cpp
@@ -5,29 +5,12 @@
 using namespace std;
 
 int solution(vector<vector<int>> triangle) {
-    const int len = triangle.size();
-    int dp[len][len];
-    
-    dp[0][0] = triangle[0][0];
-    
-    for (int r = 1; r < len; r++) {
+    // 바텀업으로 활용
+    for (int r = triangle.size() - 2; r >= 0; r--) {
         for (int c = 0; c <= r; c++) {
-            if (c == 0) {
-                dp[r][c] = dp[r-1][c] + triangle[r][c];
-            } else if (c == r) {
-                dp[r][c] = dp[r-1][c-1] + triangle[r][c];
-            } else {
-                dp[r][c] = max(dp[r-1][c-1], dp[r-1][c]) + triangle[r][c];
-            }
-            
+            triangle[r][c] += max(triangle[r+1][c],  triangle[r+1][c+1]);
         }
     }
     
-    int max_val = -1;
-    
-    for (int c = 0; c < len; c++) {
-        max_val = max(max_val, dp[len-1][c]);
-    }
-    
-    return max_val;
+    return triangle[0][0];
 }
