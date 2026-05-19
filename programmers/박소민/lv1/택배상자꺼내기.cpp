8814fad18cd8ab43482672ed38b180b45b48caf5
Sminp <0102262@gmail.com>
2026-05-19 10:19:23 +0900

    refactor: 박소민 택배상자꺼내기.cpp

--- programmers/박소민/lv1/택배상자꺼내기.cpp
+++ programmers/박소민/lv1/택배상자꺼내기.cpp
@@ -4,24 +4,31 @@
 using namespace std;
 
 int solution(int n, int w, int num) {
-    int answer = 0;
-    int max_r = (n-1) / w;
-    int max_rest = (n-1) % w;
+    // 1. num의 0-based 인덱스 및 행/열 계산
+    int target_idx = num - 1;
+    int target_row = target_idx / w;
+    int target_rest = target_idx % w;
     
-    // 1. num의 행과 열을 구한다.
-    int r = (num-1) / w;
-    int rest = (num-1) % w;
+    // 지그재그 무시하고 왼쪽 기준 절대 열 좌표로 통일
+    int target_col = (target_row % 2 == 0) ? target_rest : (w - 1 - target_rest);
     
-    // 2. num의 열의 최상단 행을 구해 answer를 갱신한다.
-    if (max_r % 2 == 0 && r % 2 == 0) {
-        answer = rest <= max_rest ? max_r - r + 1 : max_r - r;
-    } else if (max_r % 2 != 0 && r % 2 == 0) {
-        answer = (w-1) - max_rest <= rest ? max_r - r + 1 : max_r - r;
-    } else if (max_r % 2 == 0 && r % 2 != 0) {
-        answer = (w-1) - rest <= max_rest ? max_r - r + 1 : max_r - r;
+    // 2. n의 꼭대기 층 정보 계산
+    int last_idx = n - 1;
+    int max_row = last_idx / w;
+    int max_rest = last_idx % w;
+    
+    // 마지막 상자의 왼쪽 기준 절대 열 좌표
+    int last_col = (max_row % 2 == 0) ? max_rest : (w - 1 - max_rest);
+    
+    // 내 열의 꼭대기에 상자가 존재하는지 판별
+    bool has_box_on_top = false;
+    if (max_row % 2 == 0) {
+        has_box_on_top = (target_col <= last_col);
     } else {
-        answer = rest <= max_rest ? max_r - r + 1 : max_r - r;
+        has_box_on_top = (target_col >= last_col);
     }
     
-    return answer;
+    int total_height = max_row + (has_box_on_top ? 1 : 0);
+    
+    return total_height - target_row;
 }
