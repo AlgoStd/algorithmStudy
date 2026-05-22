e2c6c757db1cb58abb1648120f8da5a4a1347477
Sminp <0102262@gmail.com>
2026-05-22 12:01:32 +0900

    refactor: 박소민 파괴되지않은건물.cpp

--- programmers/박소민/lv3/파괴되지않은건물.cpp
+++ programmers/박소민/lv3/파괴되지않은건물.cpp
@@ -1,26 +1,21 @@
-#include <string>
 #include <vector>
 
 using namespace std;
 
 int solution(vector<vector<int>> board, vector<vector<int>> skill) {
-    int answer = 0;
     int n = board.size();
     int m = board[0].size();
     vector<vector <int>> arr(n+1, vector<int>(m+1, 0));
     
-    for (int i = 0; i < skill.size(); i++) {
-        if (skill[i][0] == 1) {
-            arr[skill[i][1]][skill[i][2]] -= skill[i][5];
-            arr[skill[i][3]+1][skill[i][2]] += skill[i][5];
-            arr[skill[i][1]][skill[i][4]+1] += skill[i][5];
-            arr[skill[i][3]+1][skill[i][4]+1] -= skill[i][5];
-        } else {
-            arr[skill[i][1]][skill[i][2]] += skill[i][5];
-            arr[skill[i][3]+1][skill[i][2]] -= skill[i][5];
-            arr[skill[i][1]][skill[i][4]+1] -= skill[i][5];
-            arr[skill[i][3]+1][skill[i][4]+1] += skill[i][5];
-        }
+    // const auto& 읽기 전용! C++11 이상부터
+    for (const auto& s : skill) {
+        int val = (s[0] == 1) ? -s[5] : s[5];
+        int r1 = s[1], c1 = s[2], r2 = s[3], c2 = s[4];
+        
+        arr[r1][c1] += val;
+        arr[r2 + 1][c1] -= val;
+        arr[r1][c2 + 1] -= val;
+        arr[r2 + 1][c2 + 1] += val;
     }
     
     for (int r = 0; r < n; r++) {
@@ -29,15 +24,13 @@ int solution(vector<vector<int>> board, vector<vector<int>> skill) {
         }
     }
     
-    for (int r = 1; r < board.size(); r++) {
-        for (int c = 0; c < board[0].size(); c++) {
-            arr[r][c] += arr[r-1][c];
-        }
-    }
-    
-    for (int r = 0; r < board.size(); r++) {
-        for (int c = 0; c < board[0].size(); c++) {
-            if (arr[r][c] + board[r][c] > 0) {
+    int answer = 0;
+    for (int c = 0; c < m; c++) {
+        for (int r = 0; r < n; r++) {
+            if (r > 0) {
+                arr[r][c] += arr[r-1][c];
+            }
+            if (board[r][c] + arr[r][c] > 0) {
                 answer++;
             }
         }
