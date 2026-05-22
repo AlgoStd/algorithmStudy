bae8dba0ab6d62bc019aae137823810a9812eb08
Sminp <0102262@gmail.com>
2026-05-22 17:02:39 +0900

    refactor: 박소민 영어 끝말잇기.cpp

--- programmers/박소민/lv2/영어 끝말잇기.cpp
+++ programmers/박소민/lv2/영어 끝말잇기.cpp
@@ -5,22 +5,26 @@
 using namespace std;
 
 vector<int> solution(int n, vector<string> words) {
+    if (words.empty()) return {0, 0};
+    
     unordered_set<string> seen_words;
     
+    seen_words.reserve(words.size());
     seen_words.insert(words[0]);
     
     for (int i=1; i<words.size(); i++) {
         const string &curr_word = words[i];
         const string &prev_word = words[i - 1];
         
-        if (prev_word.back() != curr_word.front()
-           || seen_words.find(curr_word) != seen_words.end()) {
-            int person = (i%n) +1;
-            int turn = (i/n)+1;
-            return {person, turn};
+        if (prev_word.back() != curr_word.front()) {
+            return {(i % n) + 1, (i / n) + 1};
         }
         
-        seen_words.insert(curr_word);
+        auto [it, inserted] = seen_words.insert(curr_word);
+        
+        if (!inserted) {
+            return {(i % n) + 1, (i / n) + 1};
+        }
     }
     
     return {0, 0};
