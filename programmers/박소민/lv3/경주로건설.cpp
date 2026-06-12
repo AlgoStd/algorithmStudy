86937a0978c9ea368294a061521df61f2d5273f9
Sminp <0102262@gmail.com>
2026-06-12 20:52:05 +0900

    refactor: 박소민 경주로건설.cpp

--- programmers/박소민/lv3/경주로건설.cpp
+++ programmers/박소민/lv3/경주로건설.cpp
@@ -1,8 +1,6 @@
-#include <string>
 #include <vector>
 #include <queue>
 #include <array>
-#include <climits>
 #include <algorithm>
 
 using namespace std;
@@ -13,53 +11,53 @@ visited를 사용하지 않고 cost를 또 사용하는 이유
 if문을 사용해서 최소 비용인 경우에만 큐에 넣으므로 시간 복잡도가 개선된다.
 */
 
+const int INF = 1e9;
+
 int solution(vector<vector<int>> board) {
     int n = board.size();
-    priority_queue<array<int, 4>> q;
     
-    vector<vector<vector<int>>> cost(n, vector<vector<int>>(n, vector<int>(2, -INT_MAX)));
+    priority_queue<array<int, 4>, vector<array<int, 4>>, greater<array<int, 4>>> pq;
+    
+    vector<vector<vector<int>>> cost(n, vector<vector<int>>(n, vector<int>(2, INF)));
     
     // 위: 0, 오: 1, 아: 0, 왼: 1 
     int dr[4] = {0, 1, 0, -1};
     int dc[4] = {1, 0, -1, 0};
     
-    q.push({0, 0, 0, 0}); // 현재 비용, 행, 열, 방향
+    pq.push({0, 0, 0, 0}); // 현재 비용, 행, 열, 방향
     cost[0][0][0] = 0;
     
-    q.push({0, 0, 0, 1});
+    pq.push({0, 0, 0, 1});
     cost[0][0][1] = 0;
     
-    while (!q.empty()) {
-        auto [c_cost, cr, cc, cd] = q.top();
-        q.pop();
+    while (!pq.empty()) {
+        auto [c_cost, cr, cc, cd] = pq.top();
+        pq.pop();
+        
+        if (cost[cr][cc][cd] < c_cost) continue;
         
         if (cr == n-1 && cc == n-1) {
-            return -c_cost;
+            return c_cost;
         }
         
         for (int i = 0; i < 4; ++i) {
-            int n_cost;
             int nr = cr + dr[i];
             int nc = cc + dc[i];
             int nd = i % 2;
             
-            if (nr < 0 || nr > n-1 || nc < 0 || nc > n-1 ||
+            if (nr < 0 || nr >= n || nc < 0 || nc >= n ||
                board[nr][nc] == 1) continue;
             
-            if (cd == nd) {
-                n_cost = c_cost - 100;
-            } else if (cd != nd) {
-                n_cost = c_cost - 600;
-            }
+            int n_cost = c_cost + ((cd == nd) ? 100 : 600);
             
-            if (n_cost > cost[nr][nc][nd]) {
-                q.push({n_cost, nr, nc, nd});
+            if (n_cost < cost[nr][nc][nd]) {
                 cost[nr][nc][nd] = n_cost;
+                pq.push({n_cost, nr, nc, nd});
             }
         }
         
     }
     
     
-    return -max(cost[n-1][n-1][0], cost[n-1][n-1][1]);
+    return min(cost[n-1][n-1][0], cost[n-1][n-1][1]);
 }
