82f66255729048b7fd586573aff27db38d84a972
Sminp <0102262@gmail.com>
2026-06-16 09:17:50 +0900

    refactor: 박소민 붕대감기.cpp

--- programmers/박소민/lv1/붕대감기.cpp
+++ programmers/박소민/lv1/붕대감기.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,37 +12,34 @@ attacks 배열을 순회하면서 상태를 변화하기 때문.
 */
 
 int solution(vector<int> bandage, int health, vector<vector<int>> attacks) {
-    int curr_health = health;
-    int t = bandage[0];
-    int x = bandage[1];
-    int y = bandage[2];
+    const int t = bandage[0];
+    const int x = bandage[1];
+    const int y = bandage[2];
     
+    int curr_health = health;
     int curr_time = 1;
     int recover_time = 0;
     
-    for (auto& attack : attacks) {
+    for (const auto& attack : attacks) {
         int attack_time = attack[0];
         int attack_amount = attack[1];
         
         // 1. 공격 시간 전까지 체력 회복
         recover_time = (attack_time - curr_time);
-        curr_health += (recover_time * x);
         
-        if (recover_time >= t) {
-            curr_health += (recover_time / t) * y;
+        if (recover_time > 0) {
+            int total_heal = (recover_time * x) + ((recover_time / t) * y);
+            curr_health = min(health, curr_health + total_heal);
         }
         
-        curr_time = attack_time + 1;
-        
-        if (curr_health >= health) {
-            curr_health = health;
-        }
-        
+        // 2. 공격 받은 후 체력 감소
         curr_health -= attack_amount;
-        
         if (curr_health <= 0) {
             return -1;
         }
+        
+        // 3. 시간 갱신
+        curr_time = attack_time + 1;
     }
     
     return curr_health;
