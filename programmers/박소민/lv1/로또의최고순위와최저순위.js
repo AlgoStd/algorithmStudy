eb203430f4fdc05e29a514be88dc7d1116c13612
Sminp <0102262@gmail.com>
2026-07-20 21:01:46 +0900

    refactor: 박소민 로또의최고순위와최저순위 길이가 매우 큰 경우 (rank도 바꿔야겠지만 여기서는 예외) Set 자료구조를 이용하여 탐색속도를 O(1)로 줄인다.

--- programmers/박소민/lv1/로또의최고순위와최저순위.js
+++ programmers/박소민/lv1/로또의최고순위와최저순위.js
@@ -1,9 +1,19 @@
 function solution(lottos, win_nums) {
   const rank = [6, 6, 5, 4, 3, 2, 1];
 
-  const zeroCnt = lottos.filter((num) => num === 0).length;
+  // 6개의 배열이 아닌 배열의 길이가 매우 크다면 -> Set
+  const winSet = new Set(win_nums);
 
-  const matchCnt = lottos.filter((num) => win_nums.includes(num)).length;
+  let zeroCnt = 0;
+  let matchCnt = 0;
+
+  for (const num of lottos) {
+    if (num === 0) {
+      zeroCnt++;
+    } else if (winSet.has(num)) {
+      matchCnt++;
+    }
+  }
 
   return [rank[matchCnt + zeroCnt], rank[matchCnt]];
 }
