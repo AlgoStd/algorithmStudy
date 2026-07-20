2e5160aefaed313566408a5961f63d5ed596f170
Sminp <0102262@gmail.com>
2026-07-20 21:00:40 +0900

    refactor: 박소민 로또의최고순위와최저순위 길이가 6으로 고정된 경우 공간복잡도 대신에 시간복잡도를 버린다.

--- programmers/박소민/lv1/로또의최고순위와최저순위.js
+++ programmers/박소민/lv1/로또의최고순위와최저순위.js
@@ -1,24 +1,9 @@
-// 최고, 최저 순위
-/*
-알아볼 수 없는 번호 0;
-*/
-
-const level = { 6: 1, 5: 2, 4: 3, 3: 4, 2: 5, 1: 6, 0: 6 };
-
 function solution(lottos, win_nums) {
-  const winSet = new Set();
+  const rank = [6, 6, 5, 4, 3, 2, 1];
 
-  for (const num of win_nums) {
-    winSet.add(num);
-  }
+  const zeroCnt = lottos.filter((num) => num === 0).length;
 
-  let sameCnt = 0;
-  let zeroCnt = 0;
+  const matchCnt = lottos.filter((num) => win_nums.includes(num)).length;
 
-  for (const num of lottos) {
-    if (num === 0) zeroCnt++;
-    else if (winSet.has(num)) sameCnt++;
-  }
-
-  return [level[String(sameCnt + zeroCnt)], level[String(sameCnt)]];
+  return [rank[matchCnt + zeroCnt], rank[matchCnt]];
 }
