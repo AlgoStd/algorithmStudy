8ff78b36656f011499e7318ab37f8378b6c6a110
Sminp <0102262@gmail.com>
2026-07-27 21:11:38 +0900

    feat: 박소민 데이터분석.cpp 이거는 ai의 도움을 ㅂ다음 sort는 algorithm과 람다함수로 활용하여 특정열을 기준으로 sort

---
+++ programmers/박소민/lv1/데이터분석.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int get_idx(string s) {
+    if (s == "code") return 0;
+    if (s == "date") return 1;
+    if (s == "maximum") return 2;
+    return 3;
+}
+
+vector<vector<int>> solution(vector<vector<int>> data, string ext, int val_ext, string sort_by) {
+    vector<vector<int>> answer;
+    
+    int target_idx = get_idx(ext);
+    int sort_idx = get_idx(sort_by);
+    
+    for (int i = 0; i < data.size(); i++) {
+        if (data[i][target_idx] < val_ext) {
+            answer.push_back(data[i]);
+        }
+    }
+    
+     sort(answer.begin(), answer.end(), [sort_idx](const vector<int>& a, const vector<int>& b){
+         return a[sort_idx] < b[sort_idx];
+     });
+    
+    return answer;
+}
