ff3f041c2eabc82c2e11f9bf005332bbcfb37b1b
Sminp <0102262@gmail.com>
2026-09-22 11:06:01 +0900

    refactor: 박소민 완전범죄.js 2차 최적화 : 2차원 배열을 온전히 1차원 배열로만 주석 확인할 것!

--- programmers/박소민/lv2/완전범죄.js
+++ programmers/박소민/lv2/완전범죄.js
@@ -9,30 +9,28 @@ function solution(info, n, m) {
   for (let i = 0; i < len; i++) {
     const [a, b] = info[i];
 
-    const nextDp = Array(n).fill(Infinity);
-
     // dp 배열을 순회하면서 이전 값을 갱신
-    for (let j = 0; j < n; j++) {
+    // 역순 + a가 먼저 훔치고 b가 훔치는 경우 갱신
+    // 이유 : dp[j]의 오염 때문
+    // (b가 이미 훔친 상황에서 a가 훔칠 수 있음)
+    for (let j = n - 1; j >= 0; j--) {
       // 이전 것이 없다 -> 없다.
       if (dp[j] === Infinity) continue;
 
       const prevB = dp[j];
 
-      // b가 훔치는 경우
-      if (prevB + b < m) {
-        // 이 부분이 이해가 안가. 왜 nextDp[j]랑 비교해야해?
-        // info[i-1]를 꼭 훔쳐야 하잖아.
-        nextDp[j] = Math.min(prevB + b, nextDp[j]);
-      }
-
       // a가 훔치는 경우
       if (j + a < n) {
-        // 이 부분도
-        nextDp[j + a] = Math.min(prevB, nextDp[j + a]);
+        dp[j + a] = Math.min(prevB, dp[j + a]);
+      }
+
+      // b가 훔치는 경우
+      if (prevB + b < m) {
+        dp[j] = prevB + b;
+      } else {
+        dp[j] = Infinity;
       }
     }
-
-    dp = nextDp;
   }
 
   for (let i = 0; i < n; i++) {
