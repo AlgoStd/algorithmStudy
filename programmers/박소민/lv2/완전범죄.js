75f6af36d51f2fb7097ce8d3ac84cb586d7568b3
Sminp <0102262@gmail.com>
2026-09-22 10:36:04 +0900

    refactor: 박소민 완전범죄.js
    1. 일차 최적화 -> 2차원 배열을 1차원 배열로
    매번 새로운 배열 생성해서 dp = 그 다음 새로운 배열로 초기화

--- programmers/박소민/lv2/완전범죄.js
+++ programmers/박소민/lv2/완전범죄.js
@@ -1,42 +1,45 @@
 function solution(info, n, m) {
-    const len = info.length;
-    
-    // len + 1, n
-    const dp = Array.from({length: len + 1}, () => Array(n).fill(Infinity));
-    
-    // 아무것도 고르지 않은 경우 당연히 A의 값도 0
-    dp[0][0] = 0;
-    
-    // info를 모두 순회해야 하므로
-    for (let i = 1; i <= len; i++) {
-        const [a, b] = info[i-1];
-        
-        // dp 배열을 순회하면서 이전 값을 갱신
-        for (let j = 0; j < n; j++) {
-            // 이전 것이 없다 -> 없다.
-            if (dp[i-1][j] === Infinity) continue;
-            
-            const prevB = dp[i - 1][j];
-            
-            // b가 훔치는 경우
-            if (prevB + b < m) {
-                dp[i][j] = Math.min(prevB + b, dp[i][j]);
-            }
-            
-            // a가 훔치는 경우
-            if (j + a < n) {
-                dp[i][j + a] = Math.min(prevB, dp[i][j + a]);
-            }
-        }
+  const len = info.length;
+
+  let dp = Array(n).fill(Infinity);
+
+  dp[0] = 0;
+
+  // info를 모두 순회해야 하므로
+  for (let i = 0; i < len; i++) {
+    const [a, b] = info[i];
+
+    const nextDp = Array(n).fill(Infinity);
+
+    // dp 배열을 순회하면서 이전 값을 갱신
+    for (let j = 0; j < n; j++) {
+      // 이전 것이 없다 -> 없다.
+      if (dp[j] === Infinity) continue;
+
+      const prevB = dp[j];
+
+      // b가 훔치는 경우
+      if (prevB + b < m) {
+        // 이 부분이 이해가 안가. 왜 nextDp[j]랑 비교해야해?
+        // info[i-1]를 꼭 훔쳐야 하잖아.
+        nextDp[j] = Math.min(prevB + b, nextDp[j]);
+      }
+
+      // a가 훔치는 경우
+      if (j + a < n) {
+        // 이 부분도
+        nextDp[j + a] = Math.min(prevB, nextDp[j + a]);
+      }
     }
-    
-    let answer = Infinity;
-    
-    for (let i = 0; i < n; i++) {
-        if (dp[len][i] < m) {
-            answer = Math.min(answer, i);
-        }
+
+    dp = nextDp;
+  }
+
+  for (let i = 0; i < n; i++) {
+    if (dp[i] < m) {
+      return i;
     }
-    
-    return answer === Infinity ? -1 : answer;
+  }
+
+  return -1;
 }
